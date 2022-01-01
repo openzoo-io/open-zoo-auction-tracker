c7175d26acc2cf9b55534f3a98fd8b22401b4627
lolieatapple <lolieatapple@gmail.com>
2022-01-01 20:45:04 +0800

    add uncaught error process

--- app.js
+++ app.js
@@ -27,4 +27,14 @@ const connect = () => {
   })
 }
 
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
 connect();
